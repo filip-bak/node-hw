581dfb1a383ab1e973830b550cb31c2bd3a88313
Filip Bączkowski <filo.b@wp.pl>
2023-10-01 21:32:24 +0200

    feat: add new errors

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,7 +2,7 @@ const ErrorHandler = (err, req, res, next) => {
   const errStatus = err.statusCode || 500
   const errMsg = err.message || 'Something went wrong'
 
-  console.error(`Error: ${errMsg}`)
+  console.error({ Error: errMsg })
 
   res.status(errStatus).json({
     message: errMsg,
--- shared/errors.js
+++ shared/errors.js
@@ -1,16 +1,60 @@
+// DATABASE
 class DatabaseConectionError extends Error {
   constructor() {
     super('Database connection failed')
   }
 }
+class UnknownDatabaseError extends Error {
+  constructor() {
+    super('Oops, something went wrong at database layer.')
+  }
+}
+
+// USER
+class NotAuthorizedError extends Error {
+  constructor() {
+    super('Not authorized')
+  }
+}
+class DuplicatedKeyError extends Error {
+  constructor() {
+    super('Email address is already in use.')
+  }
+}
+
+// GENERAL
 class NotFoundError extends Error {
   constructor() {
     super('Not found')
     this.statusCode = 404
   }
 }
+class PageNotFoundError extends Error {
+  constructor() {
+    super('This page is not Found!')
+    this.statusCode = 404
+  }
+}
+class ContactsNotFoundError extends Error {
+  constructor() {
+    super('No contacts found')
+    this.statusCode = 404
+  }
+}
+class InvalidQueryParamsError extends Error {
+  constructor() {
+    super('Invalid query parameters')
+    this.statusCode = 400
+  }
+}
 
 module.exports = {
   DatabaseConectionError,
   NotFoundError,
+  PageNotFoundError,
+  ContactsNotFoundError,
+  InvalidQueryParamsError,
+  UnknownDatabaseError,
+  DuplicatedKeyError,
+  NotAuthorizedError,
 }
