3135de2f28e7585aa3fba81058d35d234f20286c
Filip Bączkowski <filo.b@wp.pl>
2023-10-17 22:39:14 +0200

    refactor: environment variables

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const { URI, DEV_URI, ENV } = require('./config')
 
 const connect = async () => {
   try {
-    const databaseURI = ENV === 'development' ? DEV_URI : URI
+    const databaseURI = ENV === 'development' ? DEV_URI || URI : URI
 
     await mongoose.connect(databaseURI)
   } catch (err) {
